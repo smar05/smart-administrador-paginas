80ba787bb4a635afc3f0302816ff88250a6b9126
Ricardo Mantilla <mantillasanchezr@gmail.com>
2021-09-12 01:06:10 -0500

    Footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
-  exports: [NavbarComponent],
+  declarations: [NavbarComponent, FooterComponent],
+  exports: [NavbarComponent, FooterComponent],
   imports: [CommonModule],
 })
 export class SharedModule {}
