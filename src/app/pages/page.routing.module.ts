800b3eea9e3af25fdfcd86402ca2577e2175ec74
Ricardo Mantilla <mantillasanchezr@gmail.com>
2021-09-12 23:59:24 -0500

    Login
    Se crea la pagina de login y se configuran sus validaciones

--- src/app/pages/page.routing.module.ts
+++ src/app/pages/page.routing.module.ts
@@ -5,6 +5,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginModule),
+  },
+  {
     path: '',
     component: MainPageComponent,
     children: [
