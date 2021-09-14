0d4da0eabc855c63cc704280b3ae9beee370d39d
Ricardo Mantilla <mantillasanchezr@gmail.com>
2021-09-13 23:39:49 -0500

    Guard
    Guard que valide si el usuario ingreso

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return new Promise((resolve) => {
+      //Validamos que exista el token
+      if (localStorage.getItem('token') != null) {
+        resolve(true);
+      } else {
+        this.router.navigateByUrl('/login');
+        resolve(false);
+      }
+    });
+  }
+}
