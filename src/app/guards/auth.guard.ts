946f14993f532a2b7e3db6032353cf8cf69052ec
Ricardo Mantilla <mantillasanchezr@gmail.com>
2021-09-13 23:54:46 -0500

    Salir del sistema
    logout

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,12 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  RouterStateSnapshot,
-  UrlTree,
-  Router,
-} from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -14,14 +7,7 @@ import { Observable } from 'rxjs';
 export class AuthGuard implements CanActivate {
   constructor(private router: Router) {}
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
+  canActivate(): Promise<boolean> {
     return new Promise((resolve) => {
       //Validamos que exista el token
       if (localStorage.getItem('token') != null) {
--- src/app/shared/side-bar/side-bar.component.ts
+++ src/app/shared/side-bar/side-bar.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.css']
+  styleUrls: ['./side-bar.component.css'],
 })
 export class SideBarComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  //FUncion de salida del sistema
+  public logout(): void {
+    localStorage.removeItem('token');
+    localStorage.removeItem('refreshToken');
+    this.router.navigateByUrl('/login');
   }
-
 }
