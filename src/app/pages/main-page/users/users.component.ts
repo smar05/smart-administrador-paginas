f63210c1caa7149920588da7ddfaf2b931fbd285
Ricardo Mantilla <mantillasanchezr@gmail.com>
2021-10-16 17:00:26 -0500

    Interfaz de usuarios

--- src/app/pages/main-page/users/users.component.ts
+++ src/app/pages/main-page/users/users.component.ts
@@ -1,15 +1,17 @@
+import { Iusers } from './../../../interface/iusers';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
+  public users: Iusers[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(this.users);
   }
-
 }
