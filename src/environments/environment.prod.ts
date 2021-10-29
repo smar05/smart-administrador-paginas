c36c9797ef1392f8a207c08219ca523fdd3bc36a
Ricardo Mantilla <mantillasanchezr@gmail.com>
2021-10-28 21:24:54 -0500

    Deploy en el hosting de integroadmin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  urlFirebase: 'https://smar-software-default-rtdb.firebaseio.com/',
+  urlLogin:
+    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBxCouwRSjWYrt9lWcc3GhqYaeUhxYVrWc',
+  urlGetUser:
+    'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBxCouwRSjWYrt9lWcc3GhqYaeUhxYVrWc',
+  firebaseConfig: {
+    apiKey: 'AIzaSyBxCouwRSjWYrt9lWcc3GhqYaeUhxYVrWc',
+    authDomain: 'smar-software.firebaseapp.com',
+    databaseURL: 'https://smar-software-default-rtdb.firebaseio.com',
+    projectId: 'smar-software',
+    storageBucket: 'smar-software.appspot.com',
+    messagingSenderId: '474388620930',
+    appId: '1:474388620930:web:cefbaad0c4c544efe3769b',
+    measurementId: 'G-Q0QR49CGXL',
+  },
 };
