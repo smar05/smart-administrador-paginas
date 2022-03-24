e13ed482cf4497818afba28f7fcd16c7fd99746f
Ricardo Mantilla <mantillasanchezr@gmail.com>
2022-03-24 00:05:34 -0500

    Validar el nombre del producto

--- src/app/pages/main-page/products/new-product/new-product.component.ts
+++ src/app/pages/main-page/products/new-product/new-product.component.ts
@@ -1,15 +1,46 @@
+import { functions } from './../../../../helpers/functions';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-
+import '../../../../shared/spinkit/sk-cube-grid.css';
 @Component({
   selector: 'app-new-product',
   templateUrl: './new-product.component.html',
-  styleUrls: ['./new-product.component.css']
+  styleUrls: ['./new-product.component.css'],
 })
 export class NewProductComponent implements OnInit {
+  //Grupo de controles
+  public f = this.form.group({
+    name: ['', [Validators.required]],
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  //Validaciones personalizadas
+  get name() {
+    return this.f.controls.name;
   }
 
+  //Variable para validar el envio del formulario
+  public formSubmitted: boolean = false;
+
+  //Variable de precarga
+  public loadData: boolean = false;
+
+  constructor(private form: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  //Guardar producto
+  public saveProduct(): void {
+    this.formSubmitted = true;
+
+    //Validamos que el formulario este correcto
+    if (this.f.invalid) {
+      return;
+    }
+    this.loadData = true;
+  }
+
+  //Validamos el formulario
+  public invalidField(field: string): boolean {
+    return functions.invalidField(field, this.f, this.formSubmitted);
+  }
 }
