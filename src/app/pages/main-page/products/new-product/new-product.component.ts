f0ff9657c911baa315199793b719897e777d909a
Ricardo Mantilla <mantillasanchezr@gmail.com>
2022-03-24 22:15:57 -0500

    Url del producto

--- src/app/pages/main-page/products/new-product/new-product.component.ts
+++ src/app/pages/main-page/products/new-product/new-product.component.ts
@@ -1,5 +1,6 @@
+import { ProductsService } from './../../../../services/products.service';
 import { functions } from './../../../../helpers/functions';
-import { FormBuilder, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import '../../../../shared/spinkit/sk-cube-grid.css';
 @Component({
@@ -10,7 +11,18 @@ import '../../../../shared/spinkit/sk-cube-grid.css';
 export class NewProductComponent implements OnInit {
   //Grupo de controles
   public f = this.form.group({
-    name: ['', [Validators.required]],
+    name: [
+      '',
+      {
+        validators: [
+          Validators.required,
+          Validators.maxLength(50),
+          Validators.pattern(/[.\\,\\0-9a-zA-ZáéíóúñÁÉÍÓÚ ]{1,50}/),
+        ],
+        asyncValidators: [this.isRepeatProduct()],
+        updateOn: 'blur',
+      },
+    ],
   });
 
   //Validaciones personalizadas
@@ -24,7 +36,13 @@ export class NewProductComponent implements OnInit {
   //Variable de precarga
   public loadData: boolean = false;
 
-  constructor(private form: FormBuilder) {}
+  //Variable global de la url
+  public urlInput: string = '';
+
+  constructor(
+    private form: FormBuilder,
+    private productsService: ProductsService
+  ) {}
 
   ngOnInit(): void {}
 
@@ -42,5 +60,23 @@ export class NewProductComponent implements OnInit {
   //Validamos el formulario
   public invalidField(field: string): boolean {
     return functions.invalidField(field, this.f, this.formSubmitted);
+  }
+
+  //Validar que el nombre del producto no se repita
+  public isRepeatProduct(): any {
+    return (control: AbstractControl) => {
+      const name = functions.createUrl(control.value);
+      return new Promise((resolve) => {
+        this.productsService.getFilterData('url', name).subscribe((resp) => {
+          if (Object.keys(resp).length > 0) {
+            resolve({ product: true });
+            this.urlInput = '';
+          } else {
+            resolve(null);
+            this.urlInput = name;
+          }
+        });
+      });
+    };
   }
 }
