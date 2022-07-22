9f6955a9b81e95d1573c9ffaf3341f60104c9cd3
Ricardo Mantilla <mantillasanchezr@gmail.com>
2022-07-22 07:05:19 -0500

    Se arreglan las funciones de usersService

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,16 +1,71 @@
-import { HttpClient } from '@angular/common/http';
+import { Iusers } from './../interface/iusers';
+import { IQueryParams } from './../interface/i-query-params';
+import { HttpService } from './http.service';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
-  constructor(private http: HttpClient) {}
+  private urlUsers: string = 'users';
 
-  //Se toma la informacion de la coleccion de usuarios en Firebase
-  public getData(): Observable<any> {
-    return this.http.get(`${environment.urlFirebase}users.json`);
+  constructor(private httpService: HttpService) {}
+
+  /**
+   * Se toma la informacion de la coleccion de usuarios en Firebase
+   *
+   * @param {IQueryParams} [queryParams={}]
+   * @return {*}  {Observable<any>}
+   * @memberof UsersService
+   */
+  public getData(queryParams: IQueryParams = {}): Observable<any> {
+    return this.httpService.get(`${this.urlUsers}.json`, queryParams);
+  }
+
+  /**
+   * Tomar un item de usuarios
+   *
+   * @param {string} id
+   * @param {IQueryParams} [queryParams={}]
+   * @return {*}  {Observable<any>}
+   * @memberof UsersService
+   */
+  public getItem(id: string, queryParams: IQueryParams = {}): Observable<any> {
+    return this.httpService.get(`${this.urlUsers}/${id}.json`, queryParams);
+  }
+
+  /**
+   * Guardar informacion del usuario
+   *
+   * @param {Iusers} data
+   * @return {*}  {Observable<any>}
+   * @memberof UsersService
+   */
+  public postData(data: Iusers): Observable<any> {
+    return this.httpService.post(`${this.urlUsers}.json`, data);
+  }
+
+  /**
+   * Actualizar usuario
+   *
+   * @param {string} id
+   * @param {object} data
+   * @return {*}  {Observable<any>}
+   * @memberof UsersService
+   */
+  public patchData(id: string, data: object): Observable<any> {
+    return this.httpService.patch(`${this.urlUsers}/${id}.json`, data);
+  }
+
+  /**
+   * Eliminar usuario
+   *
+   * @param {string} id
+   * @return {*}  {Observable<any>}
+   * @memberof UsersService
+   */
+  public deleteData(id: string): Observable<any> {
+    return this.httpService.delete(`${this.urlUsers}/${id}.json`);
   }
 }
