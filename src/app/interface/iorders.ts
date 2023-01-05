d1ac3eb217a7e5a289d7280e7e14d24cf6eb595b
Ricardo Mantilla <mantillasanchezr@gmail.com>
2023-01-04 22:48:05 -0500

    Gestionar las Órdenes del Marketplace - Parte 3

--- src/app/interface/iorders.ts
+++ src/app/interface/iorders.ts
@@ -1,19 +1,30 @@
 export interface Iorders {
-  id: string;
-  address: string;
-  category: string;
-  city: string;
-  country: string;
-  details: string;
-  email: string;
-  image: string;
-  info: string;
-  phone: string;
-  price: string;
-  process: string;
-  product: string;
-  quantity: number;
-  status: string;
-  url: string;
-  user: string;
+  id?: string;
+  address?: string;
+  category?: string;
+  city?: string;
+  country?: string;
+  details?: string;
+  email?: string;
+  image?: string;
+  info?: string;
+  phone?: string;
+  price?: string;
+  process?: string;
+  product?: string;
+  quantity?: number;
+  status?: string;
+  url?: string;
+  user?: string;
+}
+
+export enum EnumOrderStatus {
+  pending = 'pending',
+  delivered = 'delivered',
+}
+
+export enum EnumOrderProcessStatus {
+  pending = 'pending',
+  ok = 'ok',
+  delivered = 'delivered',
 }
