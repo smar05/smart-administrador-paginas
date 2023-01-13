9efbe2f2e8d5c243ce1d2820c8068e08ca29d862
Ricardo Mantilla <mantillasanchezr@gmail.com>
2023-01-13 01:13:32 -0500

    Organizando el Nav-Bar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public logout(): void {
+    localStorage.removeItem('token');
+    localStorage.removeItem('refreshToken');
+    this.router.navigateByUrl('/login');
   }
-
 }
