890ace184350050495253538bd78fa585ae0dcdd
Ricardo Mantilla <mantillasanchezr@gmail.com>
2023-01-23 23:00:21 -0500

    Ajustes generales

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  public anioActual!: number;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.anioActual = new Date().getFullYear();
   }
-
 }
